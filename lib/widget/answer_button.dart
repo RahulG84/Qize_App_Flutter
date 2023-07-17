2d7bfc05be1bc712511817db40d0e7a60e1c61ef
Rahul Girgal <rahul4girgal@gmail.com>
2023-07-17 18:14:22 +0530

    Added : Updated Code Added

--- lib/widget/answer_button.dart
+++ lib/widget/answer_button.dart
@@ -14,6 +14,7 @@ class AnswerButton extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(10.00),
       child: Container(
+        alignment: null,
         width: double.infinity,
         child: ElevatedButton(
           onPressed: onTap,
